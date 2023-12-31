eb8c1535f211b4cb93e843630af55c49036b32ea
Mauro Alves <mauro.alves.aguiar@outlook.com>
2023-12-31 11:58:17 -0300

    API finalizada com endpoint POST e Get criados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=MiniAPI;Integrated Security=True;TrustServerCertificate=True;");
+    }
+}
